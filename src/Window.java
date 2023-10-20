13d49eb811be4f30c93ad691fa1928989927ccfa
unknown <oussama.errouji@eidia.ueuromed.org>
2023-10-20 23:27:43 +0100

    first commit

--- src/Window.java
+++ src/Window.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class Window {
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class Window extends JFrame{
+    public Window(){
+        this.setTitle("Tableau de personnes");
+        this.setSize(500, 500);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        ArrayList<Personne> personnes = new ArrayList<Personne>();
+        adder adder = new adder(personnes);
+        table_perso table =new table_perso(personnes);
+        //set column names:
+         JTable Jtable = new JTable(table);
+         Jtable.getColumnModel().getColumn(0).setHeaderValue("Nom");
+            Jtable.getColumnModel().getColumn(1).setHeaderValue("Prenom");
+            Jtable.getColumnModel().getColumn(2).setHeaderValue("Age");
+        this.add(new JScrollPane(Jtable));
+        FlowLayout layout = new FlowLayout();
+        this.setLayout(layout);
+        this.setVisible(true);
+
+
+
+        JButton add = new JButton("Ajouter");
+        this.add(add);
+        add.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                addActionPerformed(evt);
+            }
+            private void addActionPerformed(java.awt.event.ActionEvent evt) {
+                // TODO add your handling code here:
+                String nom = JOptionPane.showInputDialog(null, "Nom de la personne");
+                String prenom = JOptionPane.showInputDialog(null, "Prenom de la personne");
+                String age = JOptionPane.showInputDialog(null, "Age de la personne");
+                personnes.add(new Personne(nom, prenom, Integer.parseInt(age)));
+                table.fireTableDataChanged();
+            }
+        });
+        JButton remove = new JButton("Supprimer");
+        this.add(remove);
+        remove.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                removeActionPerformed(evt);
+            }
+            private void removeActionPerformed(java.awt.event.ActionEvent evt) {
+                // TODO add your handling code here:
+                int index = Jtable.getSelectedRow();
+                personnes.remove(index);
+                table.fireTableDataChanged();
+            }
+        });
+
+    }
+
 }
--- src/table_perso.java
+++ src/table_perso.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class table_perso {
+import javax.swing.table.AbstractTableModel;
+import java.util.ArrayList;
+
+public class table_perso extends AbstractTableModel {
+    public ArrayList<Personne> personnes;
+   table_perso(ArrayList<Personne> personnes){
+      this.personnes=personnes;
+      for(int i=0;i<personnes.size();i++){
+          this.setValueAt(personnes.get(i).getNom(), i, 0);
+            this.setValueAt(personnes.get(i).getPrenom(), i, 1);
+            this.setValueAt(personnes.get(i).getAge(), i, 2);
+      }
+
+   }
+    @Override
+    public int getRowCount() {
+        return personnes.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+       switch(columnIndex){
+           case 0:
+               return personnes.get(rowIndex).getNom();
+           case 1:
+               return personnes.get(rowIndex).getPrenom();
+           case 2:
+               return personnes.get(rowIndex).getAge();
+           default:
+               return null;
+    }
 }
+
+}
+
